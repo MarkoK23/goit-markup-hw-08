9efc515d0227ca6bba768189e5e9788ccc00e3f1
MarkoK23 <103358498+MarkoK23@users.noreply.github.com>
2022-07-14 23:59:39 +0200

    menu-mobile

--- js/menu.js
+++ js/menu.js
@@ -1,31 +1,31 @@
-// (() => {
-//   const menuBtnRef = document.querySelector("[data-menu-button]");
-//   const mobileMenuRef = document.querySelector("[data-menu]");
-
-//   menuBtnRef.addEventListener("click", () => {
-//     const expanded =
-//       menuBtnRef.getAttribute("aria-expanded") === "true" || false;
-
-//     menuBtnRef.classList.toggle("is-open");
-//     menuBtnRef.setAttribute("aria-expanded", !expanded);
-
-//     mobileMenuRef.classList.toggle("is-open");
-//   });
-// })();
-
 (() => {
-  const refs = {
-    openMenuBtn: document.querySelector(".mobile-button__menu"),
-    closeMenuBtn: document.querySelector(".mobile-button__close"),
-    menu: document.querySelector(".mob-menu"),
-    body: document.querySelector("body"),
-  };
+  const menuBtnRef = document.querySelector("[data-menu-button]");
+  const mobileMenuRef = document.querySelector("[data-menu]");
 
-  refs.openMenuBtn.addEventListener("click", toggleMenu);
-  refs.closeMenuBtn.addEventListener("click", toggleMenu);
+  menuBtnRef.addEventListener("click", () => {
+    const expanded =
+      menuBtnRef.getAttribute("aria-expanded") === "true" || false;
 
-  function toggleMenu() {
-    refs.menu.classList.toggle("is-hidden");
-    refs.body.classList.toggle("no-scroll");
-  }
+    menuBtnRef.classList.toggle("is-open");
+    menuBtnRef.setAttribute("aria-expanded", !expanded);
+
+    mobileMenuRef.classList.toggle("is-open");
+  });
 })();
+
+// (() => {
+//   const refs = {
+//     openMenuBtn: document.querySelector(".mobile-button__menu"),
+//     closeMenuBtn: document.querySelector(".mobile-button__close"),
+//     menu: document.querySelector(".mob-menu"),
+//     body: document.querySelector("body"),
+//   };
+
+//   refs.openMenuBtn.addEventListener("click", toggleMenu);
+//   refs.closeMenuBtn.addEventListener("click", toggleMenu);
+
+//   function toggleMenu() {
+//     refs.menu.classList.toggle("is-hidden");
+//     refs.body.classList.toggle("no-scroll");
+//   }
+// })();
